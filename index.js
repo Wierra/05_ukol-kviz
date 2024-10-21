7b777234eb2eb4fe76523cf9deb40df1056b6f30
Věra Juračáková <juracakova.vera@gmail.com>
2024-10-21 22:19:27 +0200

    Řešení příkladu

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const question = document.querySelector(".question")
 
+question.textContent =
+  "Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.";
+
+const answerPrompt = prompt("Odpověď:").toUpperCase()
+// odpověď dáváme do kapitálek, abychom mohli sbírat všechny formy odpovědi - bell, Bell i BeLL apod. :-)
+
+const answer = document.querySelector(".answer__text")
+answer.textContent = answerPrompt
+
+if (answerPrompt === "BELL") {
+  answer.classList.add("answer--correct");
+} else {
+  answer.classList.add("answer--wrong");
+}
